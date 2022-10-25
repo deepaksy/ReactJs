3bca3436c0369e17e0291cda33216f21ab7d1705
deepaksy <deepaksy28@gmail.com>
2022-10-25 21:24:16 +0530

    update

---
+++ src/topics/event/FunctionClick.tsx
@@ -0,0 +1,12 @@
+function FunctionClick (){
+    function clickHandler(){
+        console.log("Button Clicked")
+    }
+    return(
+        <div>
+            <button onClick={clickHandler}>Click</button>
+        </div>
+    )
+}
+
+export default FunctionClick
